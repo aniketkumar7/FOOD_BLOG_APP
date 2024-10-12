7a4e56d35d6ffa0a2b0044e1edf8a3a6682eab1b
aniketkumar7 <aniketkumar639597@gmail.com>
2024-10-12 17:03:41 +0530

    Update Server.js

--- Backend/server.js
+++ Backend/server.js
@@ -8,18 +8,6 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors());
-app.use(
-  cors({
-    origin: "https://food-blog-app-sooty.vercel.app/",
-  })
-);
-app.use((req, res, next) => {
-  res.setHeader(
-    "Content-Security-Policy",
-    "default-src 'self'; script-src 'self' blob:;"
-  );
-  next();
-});
 app.use(express.static("public"));
 
 app.use("/", require("./routes/user"));
