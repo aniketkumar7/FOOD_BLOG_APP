e6a928adc13d880c9db2d1cc31412c049645a421
aniketkumar7 <aniketkumar639597@gmail.com>
2024-10-18 21:53:46 +0530

    Update the RecipeDetails

--- Frontend/src/pages/RecipeDetails.jsx
+++ Frontend/src/pages/RecipeDetails.jsx
@@ -2,21 +2,29 @@ import profileImg from "../assets/profile.png";
 import { useLoaderData } from "react-router-dom";
 
 export default function RecipeDetails() {
+
+  /* Represents the recipe data loaded by the useLoaderData hook.*/
   const recipe = useLoaderData();
-  console.log(recipe);
+
   return (
     <>
       <div className="outer-container">
+
         <div className="profile">
           <img src={profileImg} width="50px" height="50px"></img>
           <h5>{recipe.email}</h5>
         </div>
+
         <h3 className="title">{recipe.title}</h3>
+
         <img
           src={`https://food-blog-app.onrender.com/images/${recipe.coverImage}`}
           width="220px"
-          height="200px"></img>
+          height="200px">
+        </img>
+
         <div className="recipe-details">
+
           <div className="ingredients">
             <h4>Ingredients</h4>
             <ul>
@@ -27,6 +35,7 @@ export default function RecipeDetails() {
                 ))}
             </ul>
           </div>
+
           <div className="instructions">
             <h4>Instructions</h4>
             {recipe.instructions.split("\n").map((instruction, index) => (
@@ -36,6 +45,7 @@ export default function RecipeDetails() {
               </span>
             ))}
           </div>
+          
         </div>
       </div>
     </>
