c27b85c0ee37345b3edb40ab1aca82572892261f
aniketkumar7 <aniketkumar639597@gmail.com>
2024-10-20 20:11:59 +0530

    Add comments

--- Frontend/src/components/MainNavigation.jsx
+++ Frontend/src/components/MainNavigation.jsx
@@ -1,6 +1,8 @@
 import { Outlet } from "react-router-dom"
 import Navbar from "./Navbar"
 
+// MainNavigation component
+// This component renders the main navigation for the application using the Navbar component and the Outlet component
 const MainNavigation = () => {
   return (
     <>
